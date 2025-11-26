2a47d49a862215081c653ad1ae15effba41bd0f7
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-11-26 23:23:04 +0900

    chore :: first commit

---
+++ src/test/java/com/example/sight/SightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
